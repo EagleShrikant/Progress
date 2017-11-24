b5c579f490b9d92d58bfb5e05fef1cf1dc5b29d4
SB00503408 <SB00503408@INPUHJPC13558.ATT.TechMahindra.com>
2017-11-24 15:14:25 +0530

    commited

---
+++ Test/Buyflow/src/main/java/com/att/ajsc/buyflow/Routes.java
@@ -0,0 +1,18 @@
+package com.att.ajsc.buyflow;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.att.ajsc.common.camel.AjscRouteBuilder;
+
+@Component
+public class Routes extends RouteBuilder {
+	@Autowired
+	private AjscRouteBuilder ajscRoute;
+	
+	@Override
+	public void configure() throws Exception {
+		ajscRoute.initialize(this);
+	}
+}
---
+++ Test/Buyflow/src/main/java/com/att/oce/buyflow/bean/TestBean.java
@@ -0,0 +1,11 @@
+package com.att.oce.buyflow.bean;
+
+import com.att.oce.buyflow.vo.TestBeanVO;
+
+public interface TestBean {
+	
+	TestBeanVO testMethod1();
+	
+	TestBeanVO testMethod2(TestBeanVO objTestBeanVO );
+	
+}
